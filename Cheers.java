7ec26efda4f60bb2446414b1ecff1884f978ea4d
Noa Halevi <noa.halevi@post.runi.ac.il>
2024-11-17 14:50:42 +0200

    up2

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,31 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+	    String s = args[0].toUpperCase();
+            int number = Integer.parseInt(args[1]);
+
+            for (int i = 0; i < s.length(); i++){
+                char userChar = s.charAt(i);
+
+                if ("AEFHILMNORSX".indexOf(userChar) != -1) {
+                        System.out.println("Give me an " + userChar + ": " + userChar + "!" );
+                       
+                } else {
+                        System.out.println("Give me a " + userChar + ": " + userChar +"!" );
+                }
+                        
+                }
+
+                {
+                        System.out.println("What does that spell?");  
+                }
+                
+                for (int j = 0; j < number; j++) {
+                        System.out.println(s + "!!!");
+                }
+                
+            }
+        
+
         }
-}
