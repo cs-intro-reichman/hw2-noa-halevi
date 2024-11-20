b6da4efc9d1f9adeed354dcc5eb544264bf8b840
noa-halevi <noahalv123@gmail.com>
2024-11-20 15:19:19 +0200

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -7,17 +7,17 @@ public class Collatz {
 	  if (mode.equals("v")){
 		for(int seed = 1; seed <= userNum; seed++){
 			int currentNum = seed;
-			int steps = 1;
+			int steps = 0;
 			while (currentNum != 1) {
 				System.out.print(currentNum + " "); 
 				if (currentNum % 2 == 0) { 
-					currentNum /= 2;
+					currentNum = currentNum / 2;
 				} else { 
 					currentNum = 3 * currentNum + 1;
 				}
 				steps++;
 				}
-				System.out.println(1 + " (" + steps + ")");	
+				System.out.println(1 + " (" + (steps + 1) + ")");	
 		} 	
 		System.out.println("Every one of the first " + userNum + " hailstone sequences reached 1.");
 
